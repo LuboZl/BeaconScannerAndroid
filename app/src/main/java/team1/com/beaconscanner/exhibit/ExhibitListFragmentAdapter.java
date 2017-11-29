24ebd3c6de4612b98f5fc42c02d379068b3761aa
stupel10 <jakub.vyskoc@gmail.com>
2017-11-29 17:56:52 +0100

    with LOBOZLsupport, images in List

--- app/src/main/java/team1/com/beaconscanner/exhibit/ExhibitListFragmentAdapter.java
+++ app/src/main/java/team1/com/beaconscanner/exhibit/ExhibitListFragmentAdapter.java
@@ -2,6 +2,8 @@ package team1.com.beaconscanner.exhibit;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,8 +11,15 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
 
+import team1.com.beaconscanner.BluetoothScanner;
+import team1.com.beaconscanner.ExhibitFirebase;
+import team1.com.beaconscanner.ExhibitListDataInterface;
+import team1.com.beaconscanner.MainActivity;
+import team1.com.beaconscanner.PreviewExhibit;
 import team1.com.beaconscanner.R;
 
 /**
@@ -47,6 +56,7 @@ public class ExhibitListFragmentAdapter extends ArrayAdapter<Exhibit> {
             holder = new ExhibitHolder();
             holder.exhibit_title = (TextView) row.findViewById(R.id.fragment_exhibit_row_title);
             holder.exhibit_about = (TextView) row.findViewById(R.id.fragment_exhibit_row_about);
+            holder.exhibit_image = (ImageView) row.findViewById(R.id.fragment_exhibit_image);
             holder.exhibit_distance = (TextView) row.findViewById(R.id.fragment_exhibit_row_distance);
 
             row.setTag(holder);
@@ -60,6 +70,22 @@ public class ExhibitListFragmentAdapter extends ArrayAdapter<Exhibit> {
 
         holder.exhibit_title.setText(exhibit.getTitle());
         holder.exhibit_about.setText(exhibit.getAbout());
+
+        Picasso.with(context)
+                .load( Uri.parse( exhibit.getImagePath() ) )
+//                .placeholder(R.drawable.ic_action_name)
+//                .error(R.drawable.user_placeholder_error)
+                .resize(200,200)
+                .centerCrop()
+//                .fit()
+                .into(holder.exhibit_image)
+        ;
+
+//        Drawable d = new Drawable( Uri.parse(exhibit.getImagePath() ) );
+//        holder.exhibit_image.setBackground( d );
+
+
+
         holder.exhibit_distance.setText("Rssi: "+exhibit.getRssi());
 
         return row;
